206415b2c8b6db47f21faf784523dc1d66cb39f5
Max Sultan <maxsultakent@gmail.com>
2020-09-19 14:59:20 -0600

    added message styling

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,18 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { Item } from 'semantic-ui-react';
 
+describe('Testing the customer form',()=>{
+    it('should submit on submit button press', ()=> {
+
+    })
+
+    it('should clear on clear button press', ()=> {
+
+    })
+
+    it('should show message on submit button press', ()=> {
+
+    })
+})
